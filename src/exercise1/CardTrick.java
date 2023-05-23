4a372c2375460503113856eb368acf3d39734647
Shuya-Hu <133809035+Shuya-Hu@users.noreply.github.com>
2023-05-23 16:28:40 -0400

    Edit my own info

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -49,19 +49,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Shuya.");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Be proud of myself");
+        System.out.println("-- Work hard!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Manga");
+        System.out.println("-- Feminism");
+        System.out.println("-- Outdoor activities");
+        
 
         System.out.println();
         
